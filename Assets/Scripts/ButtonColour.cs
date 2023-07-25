35e5a319e8fccfd08dd57590eedffea010a1e9d6
Jaheim <jaheimcbrown@gmail.com>
2023-07-25 00:00:07 -0500

    New Menu Scene Design With Animation.

--- Assets/Scripts/ButtonColour.cs
+++ Assets/Scripts/ButtonColour.cs
@@ -9,20 +9,31 @@ public class ButtonColour : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     private Text buttonText;
     private Color originalColor;
     public Color hoverColor;
+    private AudioSource audioSource;
 
     private void Start()
     {
         buttonText = GetComponentInChildren<Text>();
+        audioSource = GetComponent<AudioSource>();
         originalColor = buttonText.color;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         buttonText.color = hoverColor;
+        PlayAudio();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         buttonText.color = originalColor;
+    }
+
+    private void PlayAudio()
+    {
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 }
